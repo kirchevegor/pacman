1b18f63cca3c9522e8b2e33e1c9d64be10e1bad5
kirchevegor <edk15@tpu.ru>
2024-07-06 00:57:40 +1100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 
 using namespace sf;
 
-const int H = 21;
-const int W = 19;
+const int H = 21;// Высота карты
+const int W = 19;// Ширина карты
+const int ts = 25;// Размер тайла
 
-const int ts = 25;
-
-int q = 0;
-bool life = true;
-
+int q = 0;// Количество собранных очков
+bool life = true;// Состояние пакмена (жив или мёртв)
+bool isPaused = false; // Состояние паузы
+// Карта игры
 String TileMap[H] = {
 "AAAAAAAAAAAAAAAAAAA",
 "A1       A       2A",
@@ -37,240 +37,245 @@ String TileMap[H] = {
 
 class Player {
 public:
-	float frame = 0;
-	int x = 9, y = 15;
-	int newx = 0, newy = 0;
-	int rotate = 1, ti = 0;
+    float frame = 0;// Кадр анимации
+    int x = 9, y = 15;// Координаты игрока
+    int newx = 0, newy = 0;// Новые координаты игрока
+    int rotate = 1, ti = 0;// Направление и счётчик времени
 
-	void update() {
-		frame += 0.01;
-		if (frame > 5)
-			frame -= 5;
+    void update() {
+        frame += 0.01;
+        if (frame > 5)
+            frame -= 5;
 
-		ti++;
-		if (ti >= 300) {
-			switch (rotate)
-			{
-			case 1:
-				if (TileMap[y][newx + 1] != 'A')
-					newx += 1;
-				break;
-			case 2:
-				if (TileMap[y][newx - 1] != 'A')
-					newx -= 1;
-				break;
-			case 3:
-				if (TileMap[newy - 1][x] != 'A')
-					newy -= 1;
-				break;
-			case 4:
-				if (TileMap[newy + 1][x] != 'A')
-					newy += 1;
-				break;
-			}
+        ti++;
+        if (ti >= 300) {
+            switch (rotate)
+            {
+            case 1:
+                if (TileMap[y][newx + 1] != 'A')
+                    newx += 1;
+                break;
+            case 2:
+                if (TileMap[y][newx - 1] != 'A')
+                    newx -= 1;
+                break;
+            case 3:
+                if (TileMap[newy - 1][x] != 'A')
+                    newy -= 1;
+                break;
+            case 4:
+                if (TileMap[newy + 1][x] != 'A')
+                    newy += 1;
+                break;
+            }
 
-			ti = 0;
-		}
+            ti = 0;
+        }
 
-		if (TileMap[newy][newx] == ' ' || TileMap[newy][newx] == 'B') {
-			if (TileMap[newy][newx] == ' ')
-				q++;
-			
-			if (TileMap[newy][newx] == '1'
-				|| TileMap[newy][newx] == '2' || TileMap[newy][newx] == '3' || TileMap[newy][newx] == '4')
-				life = false;
+        if (TileMap[newy][newx] == ' ' || TileMap[newy][newx] == 'B') {
+            if (TileMap[newy][newx] == ' ')
+                q++;
 
-			TileMap[y][x] = 'B';
+            if (TileMap[newy][newx] == '1'
+                || TileMap[newy][newx] == '2' || TileMap[newy][newx] == '3' || TileMap[newy][newx] == '4')
+                life = false;
 
-			TileMap[newy][newx] = 'C';
+            TileMap[y][x] = 'B';
 
-			x = newx;
-			y = newy;
-		}
+            TileMap[newy][newx] = 'C';
 
-		if (newy == 9 && (newx == 0 || newx == 18)) {
-			if (newx == 0)
-				newx = 17;
-			else
-				newx = 1;
+            x = newx;
+            y = newy;
+        }
 
-			TileMap[y][x] = 'B';
-			TileMap[newy][newx] = 'C';
+        if (newy == 9 && (newx == 0 || newx == 18)) {
+            if (newx == 0)
+                newx = 17;
+            else
+                newx = 1;
 
-			x = newx;
-			y = newy;
-		}
-	}
+            TileMap[y][x] = 'B';
+            TileMap[newy][newx] = 'C';
+
+            x = newx;
+            y = newy;
+        }
+    }
 };
 
 class Enemy {
 public:
-	int x[4] = { 1, 17 , 1, 17 }, y[4] = { 1, 1, 19, 19 };
-	int newx[4] = { 0 , 0 , 0, 0 }, newy[4] = { 0, 0, 0, 0 };
-	int rotate[4] = { 1, 1, 1, 1 }, ti = 0;
+    int x[4] = { 1, 17 , 1, 17 }, y[4] = { 1, 1, 19, 19 };// Координаты врагов
+    int newx[4] = { 0 , 0 , 0, 0 }, newy[4] = { 0, 0, 0, 0 };// Новые координаты врагов
+    int rotate[4] = { 1, 1, 1, 1 }, ti = 0;// Направление врагов и счётчик времени
 
-	void update() {
-		ti++;
+    void update() {
+        ti++;
 
-		if (ti >= 300) {
-			for (int i = 0; i < 4; i++) {
-				rotate[i] = rand() % 4 + 1;
+        if (ti >= 300) {
+            for (int i = 0; i < 4; i++) {
+                rotate[i] = rand() % 4 + 1;
 
-				newx[i] = x[i];
-				newy[i] = y[i];
+                newx[i] = x[i];
+                newy[i] = y[i];
 
-				switch (rotate[i])
-				{
-				case 1:
-					if (TileMap[y[i]][newx[i] + 1] != 'A')
-						newx[i] += 1;
-					break;
-				case 2:
-					if (TileMap[y[i]][newx[i] - 1] != 'A')
-						newx[i] -= 1;
-					break;
-				case 3:
-					if (TileMap[newy[i] - 1][x[i]] != 'A')
-						newy[i] -= 1;
-					break;
-				case 4:
-					if (TileMap[newy[i] + 1][x[i]] != 'A')
-						newy[i] += 1;
-					break;
-				}
-			}
+                switch (rotate[i])
+                {
+                case 1:
+                    if (TileMap[y[i]][newx[i] + 1] != 'A')
+                        newx[i] += 1;
+                    break;
+                case 2:
+                    if (TileMap[y[i]][newx[i] - 1] != 'A')
+                        newx[i] -= 1;
+                    break;
+                case 3:
+                    if (TileMap[newy[i] - 1][x[i]] != 'A')
+                        newy[i] -= 1;
+                    break;
+                case 4:
+                    if (TileMap[newy[i] + 1][x[i]] != 'A')
+                        newy[i] += 1;
+                    break;
+                }
+            }
 
-			ti = 0;
-		}
+            ti = 0;
+        }
 
-		for (int i = 0; i < 4; i++) {
-			if (TileMap[newy[i]][newx[i]] == ' ' || TileMap[newy[i]][newx[i]] == 'B' ||
-				TileMap[newy[i]][newx[i]] == 'C') {
-				if (TileMap[newy[i]][newx[i]] == 'B')
-					TileMap[y[i]][x[i]] = 'B';
-				else if (TileMap[newy[i]][newx[i]] == ' ')
-					TileMap[y[i]][x[i]] = ' ';
-				else if (TileMap[newy[i]][newx[i]] == 'C')
-					life = false;
+        for (int i = 0; i < 4; i++) {
+            if (TileMap[newy[i]][newx[i]] == ' ' || TileMap[newy[i]][newx[i]] == 'B' ||
+                TileMap[newy[i]][newx[i]] == 'C') {
+                if (TileMap[newy[i]][newx[i]] == 'B')
+                    TileMap[y[i]][x[i]] = 'B';
+                else if (TileMap[newy[i]][newx[i]] == ' ')
+                    TileMap[y[i]][x[i]] = ' ';
+                else if (TileMap[newy[i]][newx[i]] == 'C')
+                    life = false;
 
-				if (i == 0)
-					TileMap[newy[i]][newx[i]] = '1';
-				if (i == 1)
-					TileMap[newy[i]][newx[i]] = '2';
-				if (i == 2)
-					TileMap[newy[i]][newx[i]] = '3';
-				if (i == 3)
-					TileMap[newy[i]][newx[i]] = '4';
+                if (i == 0)
+                    TileMap[newy[i]][newx[i]] = '1';
+                if (i == 1)
+                    TileMap[newy[i]][newx[i]] = '2';
+                if (i == 2)
+                    TileMap[newy[i]][newx[i]] = '3';
+                if (i == 3)
+                    TileMap[newy[i]][newx[i]] = '4';
 
-				x[i] = newx[i];
-				y[i] = newy[i];
-			}
+                x[i] = newx[i];
+                y[i] = newy[i];
+            }
 
-			if (newy[i] == 9 && (newx[i] == 0 || newx[i] == 18)) {
-				if (newx[i] == 0)
-					newx[i] = 17;
-				else
-					newx[i] = 1;
+            if (newy[i] == 9 && (newx[i] == 0 || newx[i] == 18)) {
+                if (newx[i] == 0)
+                    newx[i] = 17;
+                else
+                    newx[i] = 1;
 
-				TileMap[y[i]][x[i]] = 'B';
-				
-				if (i == 0)
-					TileMap[newy[i]][newx[i]] = '1';
-				if (i == 1)
-					TileMap[newy[i]][newx[i]] = '2';
-				if (i == 2)
-					TileMap[newy[i]][newx[i]] = '3';
-				if (i == 3)
-					TileMap[newy[i]][newx[i]] = '4';
+                TileMap[y[i]][x[i]] = 'B';
 
-				x[i] = newx[i];
-				y[i] = newy[i];
-			}
-		}
-	}
+                if (i == 0)
+                    TileMap[newy[i]][newx[i]] = '1';
+                if (i == 1)
+                    TileMap[newy[i]][newx[i]] = '2';
+                if (i == 2)
+                    TileMap[newy[i]][newx[i]] = '3';
+                if (i == 3)
+                    TileMap[newy[i]][newx[i]] = '4';
+
+                x[i] = newx[i];
+                y[i] = newy[i];
+            }
+        }
+    }
 };
 
 int main() {
-	srand(time(0));
-	RenderWindow window(VideoMode(W * ts, H * ts), "Maze!");
+    srand(time(0));
+    RenderWindow window(VideoMode(W * ts, H * ts), "Maze!");
+// Загрузка текстур
+    Texture t;
+    t.loadFromFile("C:/Pacmanlab/Paint/title.png");
+    Sprite plat(t);
 
-	Texture t;
-	t.loadFromFile("C:/Pacmanlab/Paint/title.png");
-	Sprite plat(t);
+    Texture yw;
+    yw.loadFromFile("C:/Pacmanlab/Paint/youwin.png");
+    Sprite youwin(yw);
+    youwin.setPosition(100, 210);
 
-	Texture yw;
-	yw.loadFromFile("C:/Pacmanlab/Paint/youwin.png");
-	Sprite youwin(yw);
-	youwin.setPosition(100, 210);
+    Texture yl;
+    yl.loadFromFile("C:/Pacmanlab/Paint/youlose.png");
+    Sprite youlose(yl);
+    youlose.setPosition(100, 210);
 
-	Texture yl;
-	yl.loadFromFile("C:/Pacmanlab/Paint/youlose.png");
-	Sprite youlose(yl);
-	youlose.setPosition(100, 210);
+    Player p;
+    Enemy en;
 
-	Player p;
-	Enemy en;
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
 
-	while (window.isOpen())
-	{
-		Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == Event::Closed)
-				window.close();
+            if (event.type == Event::KeyPressed) {
+                if (event.key.code == Keyboard::P) {
+                    isPaused = !isPaused;
+                }
 
-			if (q < 171 && life)
-				if (event.type == Event::KeyPressed) {
-					p.newx = p.x;
-					p.newy = p.y;
+                if (!isPaused && q < 171 && life) {
+                    p.newx = p.x;
+                    p.newy = p.y;
 
-					if (event.key.code == Keyboard::Right)
-						p.rotate = 1;
-					if (event.key.code == Keyboard::Left)
-						p.rotate = 2;
-					if (event.key.code == Keyboard::Up)
-						p.rotate = 3;
-					if (event.key.code == Keyboard::Down)
-						p.rotate = 4;
-				}
-		}
+                    if (event.key.code == Keyboard::Right)
+                        p.rotate = 1;
+                    if (event.key.code == Keyboard::Left)
+                        p.rotate = 2;
+                    if (event.key.code == Keyboard::Up)
+                        p.rotate = 3;
+                    if (event.key.code == Keyboard::Down)
+                        p.rotate = 4;
+                }
+            }
+        }
 
-		if (q < 171 && life) {
-			p.update();
-			en.update();
-		}
-		window.clear(Color::Black);
+        if (!isPaused && q < 171 && life) {
+            p.update();
+            en.update();
+        }
+        window.clear(Color::Black);
 
-		for (int i = 0; i < H; i++)
-			for (int j = 0; j < W; j++) {
-				if (TileMap[i][j] == 'A')
-					plat.setTextureRect(IntRect(0, 0, ts, ts));
-				if (TileMap[i][j] == 'C')
-					plat.setTextureRect(IntRect(ts * int(p.frame), ts * p.rotate, ts, ts));
-				if (TileMap[i][j] == ' ')
-					plat.setTextureRect(IntRect(ts, 0, ts, ts));
-				if (TileMap[i][j] == '1')
-					plat.setTextureRect(IntRect(ts * 5, ts * en.rotate[0], ts, ts));
-				if (TileMap[i][j] == '2')
-					plat.setTextureRect(IntRect(ts * 5, ts * en.rotate[1], ts, ts));
-				if (TileMap[i][j] == '3')
-					plat.setTextureRect(IntRect(ts * 5, ts * en.rotate[2], ts, ts));
-				if (TileMap[i][j] == '4')
-					plat.setTextureRect(IntRect(ts * 5, ts * en.rotate[3], ts, ts));
-				if (TileMap[i][j] == 'B')
-					continue;
+        for (int i = 0; i < H; i++)
+            for (int j = 0; j < W; j++) {
+                if (TileMap[i][j] == 'A')
+                    plat.setTextureRect(IntRect(0, 0, ts, ts));
+                if (TileMap[i][j] == 'C')
+                    plat.setTextureRect(IntRect(ts * int(p.frame), ts * p.rotate, ts, ts));
+                if (TileMap[i][j] == ' ')
+                    plat.setTextureRect(IntRect(ts, 0, ts, ts));
+                if (TileMap[i][j] == '1')
+                    plat.setTextureRect(IntRect(ts * 5, ts * en.rotate[0], ts, ts));
+                if (TileMap[i][j] == '2')
+                    plat.setTextureRect(IntRect(ts * 5, ts * en.rotate[1], ts, ts));
+                if (TileMap[i][j] == '3')
+                    plat.setTextureRect(IntRect(ts * 5, ts * en.rotate[2], ts, ts));
+                if (TileMap[i][j] == '4')
+                    plat.setTextureRect(IntRect(ts * 5, ts * en.rotate[3], ts, ts));
+                if (TileMap[i][j] == 'B')
+                    continue;
 
-				plat.setPosition(j * ts, i * ts);
-				window.draw(plat);
-			}
+                plat.setPosition(j * ts, i * ts);
+                window.draw(plat);
+            }
 
-		if (q == 171)
-			window.draw(youwin);
-		if (!life)
-			window.draw(youlose);
+        if (q == 171)
+            window.draw(youwin);
+        if (!life)
+            window.draw(youlose);
 
-		window.display();
-	}
+        window.display();
+    }
 
-	return 0;
+    return 0;
 }
